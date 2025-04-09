cfe8283d098021fbe981fdc5c250c632fb9e241a
JarYin <87222650+JarYin@users.noreply.github.com>
2025-04-09 16:04:31 +0700

    add stripe checkout and session, add terms of service page, add navigate to billing/success page

--- src/components/premium/PremiumModal.tsx
+++ src/components/premium/PremiumModal.tsx
@@ -3,12 +3,33 @@ import { Check } from "lucide-react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { Button } from "../ui/button";
 import usePremiumModal from "@/hooks/usePremiumModal";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+import { createCheckoutSession } from "./action";
 
 const premiumFeatures = ["AI tools", "Up to 3 resumes"];
 const premiumPlusFeatures = ["Infinite resumes", "Design customization"];
 
 export default function PremiumModal() {
   const { open, setOpen } = usePremiumModal();
+
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handlePremiumClick(priceId: string) {
+    try {
+      setLoading(true);
+      const redirectUrl = await createCheckoutSession(priceId);
+      window.location.href = redirectUrl!;
+    } catch (error) {
+      console.error(error);
+      toast({
+        description: "Something went wrong. Please try again later.",
+        variant: "destructive",
+      });
+    }
+  }
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="max-w-2xl">
@@ -26,7 +47,16 @@ export default function PremiumModal() {
                   <p>{feature}</p>
                 </div>
               ))}
-              <Button>Get Premium</Button>
+              <Button
+                onClick={() =>
+                  handlePremiumClick(
+                    process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY!
+                  )
+                }
+                disabled={loading}
+              >
+                Get Premium
+              </Button>
             </div>
             <div className="border-l mx-6" />
             <div className="flex w-1/2 flex-col space-y-5">
@@ -39,7 +69,17 @@ export default function PremiumModal() {
                   <p>{feature}</p>
                 </div>
               ))}
-              <Button variant={"premium"}>Get Premium</Button>
+              <Button
+                variant={"premium"}
+                onClick={() =>
+                  handlePremiumClick(
+                    process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_MONTHLY!
+                  )
+                }
+                disabled={loading}
+              >
+                Get Premium
+              </Button>
             </div>
           </div>
         </div>
