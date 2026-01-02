12c3823552828509df835e7edd404c0bbc783260
JarYin <tempest353@hotmail.com>
2026-01-02 16:03:03 +0700

    delete openai file

--- src/lib/openai.ts
+++
@@ -1,9 +0,0 @@
-import { generateText } from "ai"
-import { openai } from "@ai-sdk/openai"
-
-const { text } = await generateText({
-    model: openai("deepseek/deepseek-chat:free"),
-    prompt: "What is love?"
-})
-
-console.log(text)
